9f45b7717d2fb5408406bc820c746d746cb4197e
Md Omor <developeromor@gmail.com>
2023-04-01 01:16:36 +0600

    create testimonial section

---
+++ src/modules/Home/Features/Testimonial/Testimonial.jsx
@@ -0,0 +1,18 @@
+const Testimonial = () => {
+  return (
+    <div className="flex flex-col justify-center items-center container mx-auto pl-9 pr-8 my-[137px] ">
+      <p className="text-[#1777CB] font-secondary text-2xl ">Testimonials</p>
+      <h1 className="font-primary lg:text-[56px] lg:leading-[100%] w-full text-center my-6 bannerTitle">
+        Trusted by students in some of the best colleges
+      </h1>
+      <p className="text-[#4E4B59] font-semibold text-lg lg:w-[1048px] mb-11 text-center">
+        But don&apos;t just take our word for it. Our users have seen
+        significant improvements in their exam scores after using our app. One
+        user, Rota, saw his TOEFL score increase by 200 points after using our
+        app for just 3 weeks. Imagine what our app can do for you!
+      </p>
+    </div>
+  );
+};
+
+export default Testimonial;
