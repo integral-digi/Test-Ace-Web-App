7230df14c2eb41de3fd77e56c0aba70bcdc40ad3
Md Omor <developeromor@gmail.com>
2023-04-01 20:05:45 +0600

    faq responsive

--- src/modules/Home/Features/Faq/Faq.jsx
+++ src/modules/Home/Features/Faq/Faq.jsx
@@ -7,16 +7,16 @@ const Faq = () => {
   const [openIndex, setOpenIndex] = useState(-1);
   return (
     <div className="container mx-auto pl-9 pr-8 mt-[112px] mb-[66px]">
-      <div className="flex justify-between">
+      <div className="flex justify-between flex-wrap">
         <div className="">
-          <h1 className="bannerTitle font-primary lg:text-[56px] lg:leading-[100%] lg:w-[458px]">
+          <h1 className="bannerTitle font-primary lg:text-[56px] text-[32px] leading-[100%] lg:leading-[100%] lg:w-[458px]">
             Frequently asked questions
           </h1>
           <p className="text-[#555867] font-secondary text-base mt-6 lg:w-[407px]">
             Here are some questions we get most times, if there’s something else
             you would like to know, shoot us a message.
           </p>
-          <div className="mt-[100px]">
+          <div className="mt-[100px] hidden lg:flex">
             <Image src={chatImg} alt="chatImg" />
           </div>
         </div>
