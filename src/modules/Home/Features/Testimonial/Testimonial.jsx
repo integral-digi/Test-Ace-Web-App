b26dec374da69d4e4652adc4b70ec793d26b176d
Md Omor <developeromor@gmail.com>
2023-04-01 20:05:19 +0600

    create testimonial responsiveness

--- src/modules/Home/Features/Testimonial/Testimonial.jsx
+++ src/modules/Home/Features/Testimonial/Testimonial.jsx
@@ -1,6 +1,16 @@
 import { useState } from "react";
+import quote from "../../../../assets/quote.svg";
 import { testimonials } from "../../config/config";
 import TestimonialItem from "./TestimonialItem";
+
+// Import Swiper React components
+import { Autoplay, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import Image from "next/image";
+import "swiper/css";
+import "swiper/swiper-bundle.css";
 
 const Testimonial = () => {
   const [selectData, setselectData] = useState("");
@@ -13,59 +23,123 @@ const Testimonial = () => {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center container mx-auto pl-9 pr-8 my-[137px] ">
-      <p className="text-[#1777CB] font-secondary text-2xl ">Testimonials</p>
-      <h1 className="font-primary lg:text-[56px] lg:leading-[100%] w-full text-center my-6 bannerTitle">
-        Trusted by students in some of the best colleges
-      </h1>
-      <p className="text-[#4E4B59] font-semibold text-lg lg:w-[1048px] mb-11 text-center">
-        But don&apos;t just take our word for it. Our users have seen
-        significant improvements in their exam scores after using our app. One
-        user, Rota, saw his TOEFL score increase by 200 points after using our
-        app for just 3 weeks. Imagine what our app can do for you!
-      </p>
+    <div className="-z-50">
+      <div className="flex flex-col justify-center items-center container mx-auto pl-9 pr-8 my-[137px] ">
+        <p className="text-[#1777CB] font-secondary text-2xl ">Testimonials</p>
+        <h1 className="font-primary text-[32px] leading-[100%] lg:text-[56px] lg:leading-[100%] w-full text-center my-6 bannerTitle">
+          Trusted by students in some of the best colleges
+        </h1>
+        <p className="text-[#4E4B59] font-semibold text-lg w-full lg:w-[1048px] mb-11 text-center">
+          But don&apos;t just take our word for it. Our users have seen
+          significant improvements in their exam scores after using our app. One
+          user, Rota, saw his TOEFL score increase by 200 points after using our
+          app for just 3 weeks. Imagine what our app can do for you!
+        </p>
 
-      <div className="flex  w-full ">
-        <div className="w-[308px] shadow-[0px_40px_60px_rgba(0,0,0,0.05)] px-12 pt-[64px] h-[430px]">
-          <ul className="">
-            {testimonials?.map((data, index) => (
-              <div
-                className="flex items-center space-x-3 cursor-pointer"
-                key={index}
-              >
+        {/* it's for desktop  */}
+        <div className="lg:flex  w-full hidden ">
+          <div className="w-[308px] shadow-[0px_40px_60px_rgba(0,0,0,0.05)] px-12 pt-[64px] h-[430px]">
+            <ul className="">
+              {testimonials?.map((data, index) => (
                 <div
-                  className={`${
-                    selectData === data.name
-                      ? "bg-[#1777CB] w-3 h-3"
-                      : "bg-[#9EA8B9] w-[5px] h-[5px]"
-                  } rounded-full `}
-                ></div>
-                <li
-                  className={`block text-[#53616E] font-secondary text-base py-[22px] ${
-                    selectData === data.name
-                      ? "!text-[#1A191B] !font-primary"
-                      : null
-                  }`}
-                  onClick={() => handleClick(data.name)}
+                  className="flex items-center space-x-3 cursor-pointer"
+                  key={index}
                 >
-                  {data.name}
-                </li>
-              </div>
-            ))}
-          </ul>
-        </div>
+                  <div
+                    className={`${
+                      selectData === data.name
+                        ? "bg-[#1777CB] w-3 h-3"
+                        : "bg-[#9EA8B9] w-[5px] h-[5px]"
+                    } rounded-full `}
+                  ></div>
+                  <li
+                    className={`block text-[#53616E] font-secondary text-base py-[22px] ${
+                      selectData === data.name
+                        ? "!text-[#1A191B] !font-primary"
+                        : null
+                    }`}
+                    onClick={() => handleClick(data.name)}
+                  >
+                    {data.name}
+                  </li>
+                </div>
+              ))}
+            </ul>
+          </div>
 
-        {filteredData.length === 0
-          ? testimonials
-              .slice(0, 1)
-              .map((data, index) => <TestimonialItem data={data} key={index} />)
-          : filteredData?.map((data, index) => (
-              <TestimonialItem
-                data={data}
-                key={index}
-                handleClick={handleClick}
-              />
-            ))}
+          {filteredData.length === 0
+            ? testimonials
+                .slice(0, 1)
+                .map((data, index) => (
+                  <TestimonialItem data={data} key={index} />
+                ))
+            : filteredData?.map((data, index) => (
+                <TestimonialItem
+                  data={data}
+                  key={index}
+                  handleClick={handleClick}
+                />
+              ))}
+        </div>
+      </div>
+
+      <div className="bg-[#F6FCFF] h-[870px]  lg:hidden flex justify-center ">
+        {/* it's for mobile  */}
+        <Swiper
+          spaceBetween={30}
+          className="!shadow-none !rounded-none !h-[850px]"
+          // autoplay={{
+          //   delay: 2500,
+          //   disableOnInteraction: false,
+          // }}
+          pagination={true}
+          modules={[Autoplay, Pagination]}
+          breakpoints={{
+            // when window width is >= 1024px (desktop)
+            1024: {
+              slidesPerView: 1.5,
+            },
+            // when window width is <= 867px (mobile)
+            867: {
+              slidesPerView: 1,
+            },
+          }}
+        >
+          {testimonials?.map((data, index) => (
+            <SwiperSlide
+              className="z-50 absolute  left-0 container mx-auto  pl-9 pr-8 overflow-hidden mt-10 !shadow-none"
+              key={index}
+            >
+              <div className="">
+                <Image
+                  src={data.imageMob}
+                  alt={data.name}
+                  width={384}
+                  height={313}
+                  className="object-cover w-full h-full"
+                />
+              </div>
+
+              <div className="flex mt-6 bg-transparent space-x-5">
+                <div className="w-12 h-12">
+                  <Image src={quote} alt="quote" width={48} height={48} />
+                </div>
+
+                <div className="bg-transparent">
+                  <h1 className="text-[#1A191B] font-primary text-2xl">
+                    {data.name}
+                  </h1>
+                  <p className="text-[#555867] w-[245px] font-secondary text-base">
+                    {data.description}
+                  </p>
+                  <button className=" font-primary cursor-pointer text-base bg-[linear-gradient(252.26deg,#771228_-0.27%,#5B1BBD_100%)] px-16 py-4 rounded-[8px] text-white	mt-8">
+                    Read More
+                  </button>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
