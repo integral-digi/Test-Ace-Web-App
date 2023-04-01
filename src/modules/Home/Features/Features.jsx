03f9d9045dfac31020a228dfed189ce0013fc450
Md Omor <developeromor@gmail.com>
2023-04-01 20:06:00 +0600

    features section responsive

--- src/modules/Home/Features/Features.jsx
+++ src/modules/Home/Features/Features.jsx
@@ -9,15 +9,15 @@ import featureImg1 from "../../../assets/cutting-edge-im.svg";
 const Features = () => {
   return (
     <div className="flex flex-col justify-between items-center container mx-auto pl-9 pr-8 my-[137px]">
-      <div className="flex justify-between items-center w-full">
+      <div className="flex justify-between items-center w-full flex-wrap">
         <div className="lg:w-[657px] lg:h-[781px]">
           <Image src={featureImg1} alt="featureImg1" width="100%" />
         </div>
         <div className="">
-          <p className="font-secondary lg:text-2xl text-[#1777CB] mb-2">
+          <p className="font-secondary lg:text-2xl text-lg text-[#1777CB] mb-2">
             Revolutionary teaching methods
           </p>
-          <h1 className="font-primary lg:text-[56px] lg:leading-[150%] mb-10 bannerTitle lg:w-[681px]">
+          <h1 className="font-primary lg:text-[56px] text-[32px] leading-[150%] lg:leading-[150%] lg:mb-10 mb-6 bannerTitle lg:w-[681px]">
             Cutting-edge Artificial Intelligence technology
           </h1>
           <p className="text-[#424242] font-secondary text-lg lg:w-[652px]">
@@ -27,19 +27,19 @@ const Features = () => {
             also includes fun mini-games that reinforce what you&apos;ve learned
             and provide a break from traditional studying.
           </p>
-          <button className="text-[#2C5282] font-semibold text-lg px-4 py-3 flex items-center justify-between mt-[48px] rounded-[5px] w-[170px] relative before:-z-50 before:block before:-inset-1 before:w-[50%] before:bg-[#F6FCFF] before:absolute before:top-0 before:left-0 before:transition-all before:duration-200 before:ease-in-out hover:before:w-[100%] before:rounded-[5px]">
+          <button className="text-[#2C5282] font-semibold text-lg px-4 py-3 flex items-center justify-between mt-6 lg:mt-12 rounded-[5px] w-[170px] relative before:-z-50 before:block before:-inset-1 before:w-[50%] before:bg-[#F6FCFF] before:absolute before:top-0 before:left-0 before:transition-all before:duration-200 before:ease-in-out hover:before:w-[100%] before:rounded-[5px]">
             Learn More
             <Image src={leftArrow} alt="leftArrow" className="ml-3" />
           </button>
         </div>
       </div>
 
-      <div className="flex justify-between items-center w-full mt-12">
-        <div className="">
-          <p className="font-secondary lg:text-2xl text-[#1777CB] mb-2">
+      <div className="flex justify-between items-center w-full mt-12 flex-wrap flex-col-reverse lg:flex-row">
+        <div className="mt-10 lg:mb-0">
+          <p className="font-secondary lg:text-2xl text-lg text-[#1777CB] mb-2">
             Excellence through competition
           </p>
-          <h1 className="font-primary lg:text-[56px] lg:leading-[150%] mb-10 bannerTitle lg:w-[681px]">
+          <h1 className="font-primary lg:text-[56px] text-[32px] leading-[150%] lg:leading-[150%] lg:mb-10 mb-6 bannerTitle lg:w-[681px]">
             Challenge yourself to excellence
           </h1>
           <p className="text-[#424242] font-secondary text-lg lg:w-[652px]">
@@ -58,7 +58,7 @@ const Features = () => {
               provide support and encouragement along the way.
             </span>
           </p>
-          <button className="text-[#2C5282] font-semibold text-lg px-4 py-3 flex items-center justify-between mt-[48px] rounded-[5px] w-[170px] relative before:-z-50 before:block before:-inset-1 before:w-[50%] before:bg-[#F6FCFF] before:absolute before:top-0 before:left-0 before:transition-all before:duration-200 before:ease-in-out hover:before:w-[100%] before:rounded-[5px]">
+          <button className="text-[#2C5282] font-semibold text-lg px-4 py-3 flex items-center justify-between mt-6 lg:mt-12 rounded-[5px] w-[170px] relative before:-z-50 before:block before:-inset-1 before:w-[50%] before:bg-[#F6FCFF] before:absolute before:top-0 before:left-0 before:transition-all before:duration-200 before:ease-in-out hover:before:w-[100%] before:rounded-[5px]">
             Learn More
             <Image src={leftArrow} alt="leftArrow" className="ml-3" />
           </button>
@@ -69,15 +69,15 @@ const Features = () => {
         </div>
       </div>
 
-      <div className="flex justify-between items-center w-full mt-12">
+      <div className="flex justify-between items-center w-full mt-12 flex-wrap">
         <div className="lg:w-[657px] lg:h-[559px]">
           <Image src={featureImg3} alt="featureImg3" width="100%" />
         </div>
-        <div className="">
-          <p className="font-secondary lg:text-2xl text-[#1777CB] mb-2">
+        <div className="lg:mt-0 mt-10">
+          <p className="font-secondary lg:text-2xl text-lg text-[#1777CB] mb-2">
             Learn on the go
           </p>
-          <h1 className="font-primary lg:text-[56px] lg:leading-[150%] mb-10 bannerTitle lg:w-[681px]">
+          <h1 className="font-primary lg:text-[56px] text-[32px] leading-[150%] lg:leading-[150%] lg:mb-10 mb-6 bannerTitle lg:w-[681px]">
             Available on your favorite app store
           </h1>
           <p className="text-[#424242] font-secondary text-lg lg:w-[652px]">
