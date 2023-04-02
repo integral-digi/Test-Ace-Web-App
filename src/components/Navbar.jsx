9765eb694c9eba871f0a32b9f4dc5ddb22ac84de
Md Omor <developeromor@gmail.com>
2023-04-02 18:55:34 +0600

    Add favicon and navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,11 +36,11 @@ const Navbar = () => {
           <div
             className={
               opened
-                ? "menu w-full lg:block flex-grow lg:items-center lg:w-auto lg:px-3 px-6 lg:relative absolute lg:top-0 lg:left-0 top-[80px] -left-[0] z-50 bg-black pb-5 transition duration-[0.4s] ease-in-out"
+                ? "menu w-full lg:block flex-grow lg:items-center lg:w-auto lg:px-3 px-6 lg:relative absolute lg:top-0 lg:left-0 top-[80px] -left-[0] z-50 bg-white pb-5 transition duration-[0.4s] ease-in-out"
                 : " menu w-full lg:block flex-grow lg:items-center lg:w-auto lg:px-3 px-6 lg:relative absolute lg:top-0 lg:left-0 top-[80px] -left-[110%] transition duration-[0.4s] ease-in-out"
             }
           >
-            <ul className="flex justify-between items-center lg:flex-row flex-col">
+            <ul className="flex justify-between items-start px-9 mt-5 lg:flex-row flex-col">
               <li
                 className="text-[#424242]
                   lg:px-[36px]  font-secondary text-base  lg:p-0 py-2 cursor-pointer"
@@ -78,6 +78,11 @@ const Navbar = () => {
                 Testimonials
               </li>
             </ul>
+            <div className="order-2 lg:order-3 flex lg:hidden mx-9 my-5 items-center">
+              <button className=" font-secondary text-base bg-[linear-gradient(252.26deg,#771228_-0.27%,#5B1BBD_100%)] px-[30px] py-3 rounded-[8px] text-white	">
+                Contact Us
+              </button>
+            </div>
           </div>
         </div>
         <div className="order-2 lg:order-3 lg:flex hidden items-center">
