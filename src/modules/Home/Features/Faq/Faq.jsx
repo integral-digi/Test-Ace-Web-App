7c28618a1082090aab4547192bc7304b0ae7ea9c
Md Omor <developeromor@gmail.com>
2023-04-02 18:55:59 +0600

    edit faq section

--- src/modules/Home/Features/Faq/Faq.jsx
+++ src/modules/Home/Features/Faq/Faq.jsx
@@ -5,14 +5,15 @@ import { accordionsData } from "../../config/config";
 import FaqItem from "./FaqItem";
 const Faq = () => {
   const [openIndex, setOpenIndex] = useState(-1);
+
   return (
     <div className="container mx-auto pl-9 pr-8 mt-[112px] mb-[66px]">
       <div className="flex justify-between flex-wrap">
         <div className="">
-          <h1 className="bannerTitle font-primary lg:text-[56px] text-[32px] leading-[100%] lg:leading-[100%] lg:w-[458px]">
+          <h1 className="bannerTitle font-primary lg:text-[56px] text-[32px] leading-[100%] lg:leading-[100%] lg:w-[458px] md:w-[395px] w-full ">
             Frequently asked questions
           </h1>
-          <p className="text-[#555867] font-secondary text-base mt-6 lg:w-[407px]">
+          <p className="text-[#555867] font-secondary text-base mt-6 lg:w-[407px] md:w-[508px]">
             Here are some questions we get most times, if there’s something else
             you would like to know, shoot us a message.
           </p>
@@ -20,7 +21,7 @@ const Faq = () => {
             <Image src={chatImg} alt="chatImg" />
           </div>
         </div>
-        <div>
+        <div className="w-full lg:w-auto">
           {accordionsData?.map((accordion, index) => (
             <FaqItem
               key={index}
