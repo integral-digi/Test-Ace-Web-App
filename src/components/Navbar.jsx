4fafba1cc3fe56b27a7abc1f51bd5de4b86ce1f5
Md Omor <developeromor@gmail.com>
2023-04-04 03:55:41 +0600

    fix navbar issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Burger } from "@mantine/core";
 import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 import logo from "../assets/logo.svg";
 
@@ -18,9 +19,11 @@ const Navbar = () => {
     <div className="absolute top-0 z-50 w-full py-10 ">
       <div className="mx-auto container flex items-center justify-between flex-wrap lg:flex-nowrap px-5 lg:px-10">
         <div className="flex items-center justify-between lg:w-auto w-full lg:border-b-0  ">
-          <div className="flex items-center flex-shrink-0  mr-16 cursor-pointer">
-            <Image src={logo} alt="logo" />
-          </div>
+          <Link href="/">
+            <div className="flex items-center flex-shrink-0  cursor-pointer">
+              <Image src={logo} alt="logo" />
+            </div>
+          </Link>
 
           <div className="block lg:hidden ">
             <Burger
@@ -40,40 +43,40 @@ const Navbar = () => {
                 : " menu w-full lg:block flex-grow lg:items-center lg:w-auto lg:px-3 px-6 lg:relative absolute lg:top-0 lg:left-0 top-[80px] -left-[110%] transition duration-[0.4s] ease-in-out"
             }
           >
-            <ul className="flex justify-between items-start px-9 mt-5 lg:flex-row flex-col">
+            <ul className="flex justify-between lg:space-x-[30px] 2xl:space-x-9 items-start px-9  lg:flex-row flex-col">
               <li
                 className="text-[#424242]
-                  lg:px-[36px]  font-secondary text-base  lg:p-0 py-2 cursor-pointer"
+                  lg:px-4 font-secondary text-base  lg:p-0 py-2 cursor-pointer"
               >
                 Home
               </li>
               <li
                 className="text-[#424242]
-                 lg:px-[36px]  font-secondary text-base lg:p-0 py-2 cursor-pointer"
+                 lg:px-4 font-secondary text-base lg:p-0 py-2 cursor-pointer"
               >
                 Projects
               </li>
               <li
                 className="text-[#424242]
-                   lg:px-[36px]  font-secondary text-base lg:p-0 py-2 cursor-pointer"
+                   lg:px-4 font-secondary text-base lg:p-0 py-2 cursor-pointer"
               >
                 App Updates
               </li>
               <li
                 className="text-[#424242]
-                lg:px-[36px]  font-secondary text-base lg:p-0 py-2 cursor-pointer"
+                lg:px-4 font-secondary text-base lg:p-0 py-2 cursor-pointer"
               >
                 Media
               </li>
               <li
                 className="text-[#424242]
-                lg:px-[36px]  font-secondary text-base   lg:p-0 py-2 cursor-pointer"
+                lg:px-4 font-secondary text-base   lg:p-0 py-2 cursor-pointer"
               >
                 Press Kit
               </li>
               <li
                 className="text-[#424242]
-                lg:px-[36px]  font-secondary text-base   lg:p-0 py-2 cursor-pointer"
+                lg:px-4 font-secondary text-base   lg:p-0 py-2 cursor-pointer"
               >
                 Testimonials
               </li>
