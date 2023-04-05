a4e3ee1ddc46fc8baf5a9d58bddd84b33cdcdc2f
anymail4eze <anymail4eze@proton.me>
2023-04-06 00:40:32 +0600

    change navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,11 @@ const Navbar = () => {
   };
 
   return (
-    <div className="absolute lg:top-14 top-32 w-full py-10 z-50">
+    <div
+      className={`absolute lg:top-14 top-32 w-full py-10 z-50  ${
+        opened ? "bg-white" : "bg-transparent"
+      }`}
+    >
       <div className="mx-auto container flex items-center justify-between flex-wrap lg:flex-nowrap px-9 lg:px-10">
         <div className="flex items-center justify-between lg:w-auto w-full lg:border-b-0 ">
           <Link href="/">
@@ -39,49 +43,49 @@ const Navbar = () => {
           <div
             className={
               opened
-                ? "menu w-full lg:block flex-grow lg:items-center lg:w-auto lg:px-3 px-6 lg:relative absolute lg:top-0 lg:left-0 top-[80px] -left-[0] z-50 bg-white pb-5 transition duration-[0.4s] ease-in-out"
+                ? "menu w-full lg:block flex-grow lg:items-center lg:w-auto lg:px-3 px-0 lg:relative absolute lg:top-0 lg:left-0 top-[110px] -left-[0] z-50 bg-white pb-5 transition duration-[0.4s] ease-in-out"
                 : " menu w-full lg:block flex-grow lg:items-center lg:w-auto lg:px-3 px-6 lg:relative absolute lg:top-0 lg:left-0 top-[80px] -left-[110%] transition duration-[0.4s] ease-in-out"
             }
           >
-            <ul className="flex justify-between lg:space-x-[30px] 2xl:space-x-9 items-start px-9  lg:flex-row flex-col">
+            <ul className="flex justify-between lg:space-x-[30px] 2xl:space-x-9 items-start   lg:flex-row flex-col ">
               <li
                 className="text-[#424242]
-                  lg:px-4 font-secondary text-base  lg:p-0 py-2 cursor-pointer"
+                  lg:px-3 font-secondary text-base  lg:p-0 cursor-pointer border-y border-[#F2F4F7] w-full px-9 py-4 lg:w-auto"
               >
                 Home
               </li>
               <li
                 className="text-[#424242]
-                 lg:px-4 font-secondary text-base lg:p-0 py-2 cursor-pointer"
+                 lg:px-3 font-secondary text-base lg:p-0 cursor-pointer border-y border-[#F2F4F7] w-full px-9 py-4 lg:w-auto"
               >
                 Projects
               </li>
               <li
                 className="text-[#424242]
-                   lg:px-4 font-secondary text-base lg:p-0 py-2 cursor-pointer"
+                   lg:px-3 font-secondary text-base lg:p-0 cursor-pointer border-y border-[#F2F4F7] w-full px-9 py-4 lg:w-auto"
               >
                 App Updates
               </li>
               <li
                 className="text-[#424242]
-                lg:px-4 font-secondary text-base lg:p-0 py-2 cursor-pointer"
+                lg:px-3 font-secondary text-base lg:p-0 cursor-pointer border-y border-[#F2F4F7] w-full px-9 py-4 lg:w-auto"
               >
                 Media
               </li>
               <li
                 className="text-[#424242]
-                lg:px-4 font-secondary text-base   lg:p-0 py-2 cursor-pointer"
+                lg:px-3 font-secondary text-base   lg:p-0 cursor-pointer border-y border-[#F2F4F7] w-full px-9 py-4 lg:w-auto"
               >
                 Press Kit
               </li>
               <li
                 className="text-[#424242]
-                lg:px-4 font-secondary text-base   lg:p-0 py-2 cursor-pointer"
+                lg:px-3 font-secondary text-base   lg:p-0 cursor-pointer border-y border-[#F2F4F7] w-full px-9 py-4 lg:w-auto"
               >
                 Testimonials
               </li>
             </ul>
-            <div className="order-2 lg:order-3 flex lg:hidden mx-9 my-5 items-center">
+            <div className="order-2 lg:order-3 flex lg:hidden mx-9 my-[50px] items-center">
               <button className=" font-secondary text-base bg-[linear-gradient(252.26deg,#771228_-0.27%,#5B1BBD_100%)] px-[30px] py-3 rounded-[8px] text-white	">
                 Contact Us
               </button>
