c799af3fdb0abb2e0d7db734a534f79b56eb6995
anymail4eze <anymail4eze@proton.me>
2023-04-06 00:41:10 +0600

    change  texts

--- src/modules/Home/config/config.js
+++ src/modules/Home/config/config.js
@@ -7,17 +7,17 @@ export const accordionsData = [
   {
     title: "How does Test Ace work?",
     content:
-      "Our app uses a custom GPT4-trained model to generate teaching materials that are tailored to your needs and level of understanding. You'll also earn points, badges, and rewards as you complete lessons and quizzes, motivating you to learn more and stay engaged. Our app also includes fun mini-games that reinforce what you've learned and provide a break from traditional studying.",
+      "Our product uses custom Artificial Intelligence trained language models to generate materials that are tailor suited for your needs and your English level. You'll also earn points, badges, and practice scores as you progress, helping you to monitor your growth. Our app also includes flashcards and review mode that reinforce what you've learned and provide a break from the hard stuff.",
   },
   {
     title: "How much does Test Ace cost?",
     content:
-      "We offer affordable pricing plans that give you access to premium features without breaking the bank. You can choose a plan that fits your budget and learning needs",
+      "Test-Ace is completely free! But we also offer a $10 Premium Membership for unlimited Flashcards, Progression mode, Review Mode, and Scholar Mode. Sign up to see if you can try Premium Membership for free!",
   },
   {
     title: "Can Test Ace really improve my exam scores?",
     content:
-      "Yes! Our users have seen significant improvements in their exam scores after using our app. Our custom teaching materials and gamified learning approach can help you learn faster and more effectively.",
+      "Yes! While we are still a new organization we are confident that whether you are a beginner just getting started, or an advanced student hoping to add a few more points to nail the perfect score, our gamified learning approach can help you learn faster and more effectively than what the traditional companies have to offer.",
   },
   {
     title: "Is my data secure with Test Ace?",
@@ -39,14 +39,14 @@ export const testimonials = [
     id: 2,
     name: "Shing",
     description:
-      "Thanks Test Extraordinaire for the SAT prep courses I took before applying to UC Irvine. Rainbow is a very knowledgeable, thoughtful, and experienced instructor who helped me understand the test and gave me strategies to use on test day. Thanks to her guidance, I was able to....",
+      "Thanks Test Extraordinaire for the SAT prep courses I took before applying to colleges in the US. Rainbow is a very knowledgeable, thoughtful, and experienced instructor who helped me understand the test and gave me strategies to use on test day. Thanks to her guidance, I was able to....",
     image: "/assets/shing.svg",
     imageMob: "/assets/shingMob.svg",
   },
   {
     id: 3,
     name: "Jamie",
-    description: "Good teachers. Thank you for everything!",
+    description: "Great teachers. Thank you for everything!",
     image: "/assets/jamie.svg",
     imageMob: "/assets/jamieMob.svg",
   },
@@ -54,7 +54,7 @@ export const testimonials = [
     id: 4,
     name: "Jane",
     description:
-      " This app is a game changer. I was able to get my score up significantly in a short amount of time. Thanks to Test Ace",
+      "This app is a game changer. I was able to get my score up significantly in a short amount of time. Thanks to Test Ace!",
     image: "/assets/jane.svg",
     imageMob: "/assets/janeMob.svg",
   },
